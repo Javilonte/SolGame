cb9dfbccd96b64c1b42c0ae2d08ca249ffc32b40
TwinJax <javi@twinchat.com.mx>
2022-02-16 13:54:17 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+How to play : 
+
+1. Go to Remix which is an online IDE designed for Solidity.
+2. Copy the source code from RockPaperScissors.sol, locate it under the browser folder.
+3. Compile the code by clicking "Start to compile" button under "Compile" tab on the right pane.
+4. Navigate to "Run" tab, choose JavaScript VM environment.
+5. Press the deploy button in order to deploy the contract.
+6. You will now see a contract under "Deployed Contracts" section.
+7. Then choose any account from "Account" drop-down list. This account will be the player 1 for the game.
+8. After choosing a player, join the game by pressing the "join" button. Make sure you pay the stake of 1 ether. This can be done by typing '1' next to "Value" and choose "ether" in the drop-down menu.
+9. Change account and join the game, this will be player 2.
+10. Now press "makeChoice" button to make a choice. A valid choice will be one of "R", "P", "S".
+11. Repeat again with the player 2 account.
+12. When you want to finish the game you have to click on the disclose button to get the game result (and only can be done by a player).
+13. Once the outcome of the game is disclosed, you will gain/lose 1 ether unless you draw.
---
+++ SolGame.sol
@@ -0,0 +1,129 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+// The game of RockPaperScissors
+contract RockPaperScissors {
+    
+    // There are two players in this game and each of them makes a choice
+    address payable public player1;
+    address payable public player2;
+    //Strings are private so that way others address cannot see their choice
+    string private choiceOfPlayer1;
+    string private choiceOfPlayer2;
+    bool private hasPlayer1MadeChoice;
+    bool private hasPlayer2MadeChoice;
+    
+    // When a player joins the game, they have to pay a playing fee 
+    uint256 public stake; //the stake should be visible to Player2
+
+    // A matrix containing result of the game depedning on its states
+    mapping(string => mapping(string => uint8)) private states;
+
+    event winnerChickenDinner (address indexed whoWon, bool winner);
+    // The constructor initialise the game environment
+    constructor() public {
+
+        states['R']['R'] = 0;
+        states['R']['P'] = 2;
+        states['R']['S'] = 1;
+        states['P']['R'] = 1;
+        states['P']['P'] = 0;
+        states['P']['S'] = 2;
+        states['S']['R'] = 2;
+        states['S']['P'] = 1;
+        states['S']['S'] = 0;
+
+        stake = 1 ether;
+    }
+    
+    // Modifiers
+    
+    modifier isJoinable() {
+        require(player1 == address(0) || player2 == address(0),
+                "The room is full."
+        );
+        require((player1 != address(0) && msg.value == stake) || (player1 == address(0)), //Player1 can choose the stake, Player2 has to match. 
+                "You must pay the stake to play the game."
+        );
+        _;
+    }
+    
+    modifier isPlayer() {
+        require(msg.sender == player1 || msg.sender == player2,
+                "You are not playing this game."
+        );
+        _;
+    }
+    
+    modifier isValidChoice(string memory _playerChoice) {
+        require(keccak256(bytes(_playerChoice)) == keccak256(bytes('R')) ||
+                keccak256(bytes(_playerChoice)) == keccak256(bytes('P')) ||
+                keccak256(bytes(_playerChoice)) == keccak256(bytes('S')) ,
+                "Your choice is not valid, it should be one of R, P and S."
+        );
+        _;
+    }
+    
+    modifier playersMadeChoice() {
+        require(hasPlayer1MadeChoice && hasPlayer2MadeChoice,
+                "The player(s) have not made their choice yet."
+        );
+        _;
+    }
+
+    // Functions
+     
+    function join() external payable 
+        isJoinable() // To join the game, there must be a free space
+    {
+        if (player1 == address(0)) {
+            player1 = payable(msg.sender);
+            stake = msg.value; //Player1 determines the stake
+            
+        } else
+            player2 = payable(msg.sender);
+    }
+    
+    function makeChoice(string calldata _playerChoice) external 
+        isPlayer()                      // Only the players can make the choice
+        isValidChoice(_playerChoice)    // The choices should be valid 
+    {
+        if (msg.sender == player1 && !hasPlayer1MadeChoice) {
+            choiceOfPlayer1 = _playerChoice;
+            hasPlayer1MadeChoice = true;
+        } else if (msg.sender == player2 && !hasPlayer2MadeChoice) {
+            choiceOfPlayer2 = _playerChoice;
+            hasPlayer2MadeChoice = true;
+        }
+    }
+    
+    function disclose() external 
+        isPlayer()          // Only players can disclose the game result
+        playersMadeChoice() // Can disclose the result when choices are made
+    {
+        // Disclose the game result
+        int result = int(uint(states[choiceOfPlayer1][choiceOfPlayer2]));
+        if (result == 0) { //if both players draw they both transfer the stake
+            player1.transfer(stake); 
+            player2.transfer(stake);
+        } else if (result == 1) { //If player 2 wins transfers the stake
+            player1.transfer(address(this).balance);
+            emit winnerChickenDinner(msg.sender, true);
+        } else if (result == 2) { //If player 1 wins transfers the stake
+            player2.transfer(address(this).balance);
+            emit winnerChickenDinner(msg.sender, true);
+        }
+        
+        // Here we reset the data of the game
+        player1 = payable(address(0));
+        player2 =  payable(address(0)) ;
+
+        choiceOfPlayer1 = "";
+        choiceOfPlayer2 = "";
+        
+        hasPlayer1MadeChoice = false;
+        hasPlayer2MadeChoice = false;
+        
+        stake = 1 ether;
+    }
+}
